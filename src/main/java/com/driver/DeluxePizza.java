7492ae789888e3678f8ddc9bfc86d633c4e1cc5e
Somendrat22 <somendrat22@gmail.com>
2024-01-18 23:07:37 +0530

    Pizza bill app

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,14 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        this.extraCheesePrice = 80;
+        if(isVeg){
+            this.extraTopingPrice = 70;
+        }else{
+            this.extraTopingPrice = 120;
+        }
+
+
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,35 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int basePrice;
+
+    public int extraTopingPrice;
+
+    private int basicExtraToppingPrice;
+
+    public int extraCheesePrice;
+
+    private int basicExtraCheesePrice;
+
+    private int papperBagPrice;
+
+    private boolean takeAway;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.basicExtraCheesePrice = 80;
+        this.papperBagPrice = 20;
+        if(isVeg){
+            this.basePrice = 300;
+            this.price = this.basePrice;
+            this.basicExtraToppingPrice = 70;
+        }else{
+            this.basePrice = 400;
+            this.price = this.basePrice;
+            this.basicExtraToppingPrice = 120;
+        }
+        this.bill = "Base Price Of The Pizza: " + this.basePrice + "\n";
     }
 
     public int getPrice(){
@@ -16,19 +42,36 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
+        this.extraCheesePrice += this.basicExtraCheesePrice;
         // your code goes here
     }
 
     public void addExtraToppings(){
+        this.extraTopingPrice += this.basicExtraToppingPrice;
         // your code goes here
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.takeAway = true;
     }
 
     public String getBill(){
         // your code goes here
+        if(this.extraCheesePrice != 0) {
+            this.price += this.extraCheesePrice;
+            this.bill += "Extra Cheese Added: " + this.extraCheesePrice + "\n";
+        }
+        if(this.extraTopingPrice != 0) {
+            this.price += this.extraTopingPrice;
+            this.bill += "Extra Toppings Added: " + this.extraTopingPrice + "\n";
+        }
+        if(this.takeAway){
+            this.price += this.papperBagPrice;
+            this.bill += "Paperbag Added: " + this.papperBagPrice + "\n";
+        }
+
+        this.bill += "Total Price: " + this.price + "\n";
         return this.bill;
     }
 }
